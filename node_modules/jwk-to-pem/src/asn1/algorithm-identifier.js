6693006d66e1116e45d142cc8bde027e6bd4af13
EC2 Default User <ec2-user@ip-172-31-32-220.us-west-2.compute.internal>
2020-06-28 00:14:22 +0000

    cleanup and remove node_modules

--- node_modules/jwk-to-pem/src/asn1/algorithm-identifier.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = require('asn1.js').define('AlgorithmIdentifer', /* @this */ function() {
-	this.seq().obj(
-		this.key('algorithm').objid(),
-		this.key('parameters').optional().any()
-	);
-});
--- node_modules/jwk-to-pem/src/asn1/version.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = require('asn1.js').define('Version', /* @this */ function() {
-	this.int();
-});
